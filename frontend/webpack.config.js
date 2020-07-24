09fcd2719cffab13696d03a06ea51e95d22b5f6f
JP <jpeters@andculture.com>
2020-07-24 09:42:06 -0400

    removed presentation project, set up webpack server

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,19 +1,22 @@
+const path = require("path");
+const webpack = require("webpack");
+
 module.exports = {
-    mode: "production",
+    entry: "./src/index.tsx",
+    mode: "development",
 
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx"]
+        extensions: [".ts", ".tsx", ".js"]
     },
 
     module: {
         rules: [
             {
                 test: /\.ts(x?)$/,
-                exclude: /node_modules/,
                 use: [
                     {
                         loader: "ts-loader"
@@ -28,13 +31,14 @@ module.exports = {
             }
         ]
     },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/"
     }
 };
