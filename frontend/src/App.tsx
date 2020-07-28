3e401013c7c1127876d434ca14caa4e734b270c1
JP <jpeters@andculture.com>
2020-07-28 12:06:50 -0400

    added path aliases

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React    from "react";
+import { HelloPage } from "@pages/hello-page";
+
 import "./sass/app.scss";
-import { HelloPage } from "./pages/hello-page";
 
 export const App = () => {
     return (
