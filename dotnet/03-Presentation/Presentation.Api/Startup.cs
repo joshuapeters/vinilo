cb8ac4edf3ae277c933d7c45586e82df0e4f52e1
JP <jpeters@andculture.com>
2020-07-29 17:00:03 -0400

    added swagger

--- dotnet/03-Presentation/Presentation.Api/Startup.cs
+++ dotnet/03-Presentation/Presentation.Api/Startup.cs
@@ -26,6 +26,9 @@ namespace Presentation.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +38,16 @@ namespace Presentation.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vinilo API V1");
+            });
 
             app.UseHttpsRedirection();
 
