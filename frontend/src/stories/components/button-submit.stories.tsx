fad3b4786fcfbd0c22fff58e46bf2915961b4d1a
JP <jpeters@andculture.com>
2021-01-11 15:34:51 -0500

    fixed storybook routs

--- frontend/src/stories/components/button-submit.stories.tsx
+++ frontend/src/stories/components/button-submit.stories.tsx
@@ -1,7 +1,6 @@
-import React, { ComponentProps } from 'react';
-import { Story, Meta }           from '@storybook/react/types-6-0';
-
+import React, { ComponentProps }           from 'react';
 import { SubmitButton, SubmitButtonProps } from "@atoms/button-submit";
+import { Story, Meta }                     from '@storybook/react/types-6-0';
 import { BUTTON_SIZE, BUTTON_TYPE }        from "@core/enums/components/buttons/button-enums";
 
 const ButtonLabel = "Submit";
